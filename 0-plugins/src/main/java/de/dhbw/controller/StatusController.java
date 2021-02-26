7b4dbdac542f879d7c20361088c9d385b8c6da17
Jeanne Helm <jeanne.helm@diva-e.com>
2021-02-26 11:06:33 +0100

    add a status controller for checking the connection to the api

---
+++ 0-plugins/src/main/java/de/dhbw/controller/StatusController.java
@@ -0,0 +1,15 @@
+package de.dhbw.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/status")
+public class StatusController {
+
+    @GetMapping
+    public String status() {
+        return "Up and running!";
+    }
+}
