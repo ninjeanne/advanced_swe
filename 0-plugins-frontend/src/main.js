9b7a0d6e36be49a1f93048ecd8b35f1b9122bc48
Jeanne Helm <jeanne.helm@diva-e.com>
2021-02-27 15:47:36 +0100

    integrate sockets to frontend

--- 0-plugins-frontend/src/main.js
+++ 0-plugins-frontend/src/main.js
@@ -3,7 +3,13 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
+import VueSocketIO from "vue-socket.io";
 
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: "http://localhost:7000",
+  options: {path: "/my-app/"} //Optional options
+}));
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
